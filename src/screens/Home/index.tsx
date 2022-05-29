28eafdd0ed1a10241d637dd8e69b88f2efc68bb0
Matheus Schneider Camilo <matheus.schneider.camilo@gmail.com>
2022-05-29 14:17:13 -0300

    Add card for adding a plan

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StackScreenProps as Props } from '@react-navigation/stack';
 import { StackParameters } from '../../routes/types';
 import { HeaderText, MainContainer, TopContainer } from './styles';
 import BottomNavigation from '../../components/BottomNavigation';
-import WorkoutCard from '../../components/WorkoutCard';
 import { Database } from '../../services/database';
+import WorkoutCard from '../../components/WorkoutCard';
+import { useIsFocused } from '@react-navigation/native';
+import { Plan } from '../../types';
 
-const Home = ({ navigation, route }: Props<StackParameters, 'Home'>) => {
+const Home = ({}: Props<StackParameters, 'Home'>) => {
+  const isFocused = useIsFocused();
+  const [plan, setPlan] = useState<Plan | undefined>(undefined);
+
+  useEffect(() => {
+    if (isFocused) {
+      setPlan(Database.getActivePlan());
+    }
+  }, [isFocused]);
+
   return (
     <TopContainer>
       <MainContainer>
         <HeaderText>My Plan</HeaderText>
-        {
-          // Using the exercises just as an example
-          Database.getExercises()
-            .slice(0, 10)
-            .map(({ name, normalized_name }) => (
-              <WorkoutCard title={name} key={normalized_name} />
-            ))
-        }
+        {plan ? (
+          <></>
+        ) : (
+          <WorkoutCard
+            title="You don't have a plan yet!"
+            subtitle="Click here to create one."
+            icon="plus"
+          />
+        )}
       </MainContainer>
       <BottomNavigation />
     </TopContainer>
